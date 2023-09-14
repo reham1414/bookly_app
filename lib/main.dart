43062bdc4c0ddc93ae9b7c3b2c4d16495e353041
reham1414 <rehamhelal981@gmail.com>
2023-09-15 00:05:08 +0300

    create app structure and create splash view and add get to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -9,7 +10,7 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
     );
   }
